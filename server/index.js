8c91b8e842f438c11c8196a5f48e43cc198d4cc6
aqilusman45 <aqilusman45@gmail.com>
2021-07-20 02:14:44 +0500

    refactor: change req

--- server/index.js
+++ server/index.js
@@ -1,23 +1,28 @@
-const express = require("express");
-const cypress = require("cypress");
+const express = require('express')
+const cypress = require('cypress')
 
-const app = express();
+const app = express()
 
-app.get("/run-test", cypressController);
+app.get('/run-test', cypressController)
 
-async function cypressController(req, res) {
+async function cypressController (req, res) {
   try {
+    res.json({
+      status: 'test execution started'
+    })
+
     const results = await cypress.run({
       config: {},
-      env: {},
-    });
-    res.json(results);
+      env: {}
+    })
+
+    console.log(results)
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
 }
 
 module.exports = (opts = {}, cb) => {
-  const port = opts.port || 3000;
-  return app.listen(port, cb);
-};
+  const port = opts.port || 3000
+  return app.listen(port, cb)
+}
