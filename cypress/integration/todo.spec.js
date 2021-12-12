8977c50971456ea17887722e86031c0213e13cae
aqilusman45 <aqilusman45@gmail.com>
2021-12-12 17:55:15 +0000

    feat(integrations): remove uneeded tests BREAKING CHANGE: the falsy integrations have been removed and will not be checked further, code on app needs be accordingly.

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -4,12 +4,4 @@ describe('cypress test', () => {
   it('works', () => {
     expect(true).to.be.true
   })
-
-  it('checks null', () => {
-    expect(null).to.be.null
-  })
-
-  it('checks string', () => {
-    expect('hello').to.be.string('hello')
-  })
 })
