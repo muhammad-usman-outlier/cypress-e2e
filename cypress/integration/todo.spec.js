bac12786c7ddbe31cdc159c9b8f5ff5032e24706
aqilusman45 <aqilusman45@gmail.com>
2021-12-12 17:51:02 +0000

    feat: add new test

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -4,7 +4,12 @@ describe('cypress test', () => {
   it('works', () => {
     expect(true).to.be.true
   })
+
   it('checks null', () => {
     expect(null).to.be.null
+  })
+
+  it('checks string', () => {
+    expect('hello').to.be.string('hello')
   })
 })
