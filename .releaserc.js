d602bc06209676fcc091eaef41676896e4544ba7
aqilusman45 <aqilusman45@gmail.com>
2021-12-12 16:54:41 +0000

    feat: semantic-release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,21 +1,12 @@
 module.exports = {
   plugins: [
-    "@semantic-release/commit-analyzer",
     "@semantic-release/github",
     ["@semantic-release/npm", {
       npmPublish: false
     }],
-    ["@semantic-release/release-notes-generator", {
-      "parserOpts": {
-        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-      },
-      "writerOpts": {
-        "commitsSort": ["subject", "scope"]
-      }
-    }],
     ["@semantic-release/git", {
       "assets": ["package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      "message": "chore(release): ${nextRelease.version}"
     }]
   ],
   branches: ['main'],
