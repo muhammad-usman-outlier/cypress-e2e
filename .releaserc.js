ca451df9aa553ca0fe1b66b8b2f39edca361a3a7
aqilusman45 <aqilusman45@gmail.com>
2021-12-12 17:38:52 +0000

    fix: refactor release

--- .releaserc.js
+++ .releaserc.js
@@ -15,7 +15,7 @@ module.exports = {
     }],
     ["@semantic-release/git", {
       "assets": ["package.json"],
-      "message": "chore(release): ${nextRelease.version}"
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }]
   ],
   branches: ['main'],
