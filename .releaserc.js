f55b13d91b3a1fc2e325eae357f5ff349054da01
aqilusman45 <aqilusman45@gmail.com>
2021-12-12 10:57:42 +0000

    feat: update version in package.json

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,21 @@
 module.exports = {
   plugins: [
     [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/github",
+
       "@semantic-release/npm",
       {
         npmPublish: false,
       },
+      [
+        "@semantic-release/git",
+        {
+          assets: ["package.json"],
+          message:
+            "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        },
+      ],
     ],
   ],
   branches: ["main"],
