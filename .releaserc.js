ef268ab977929d457fd1cc5b96af02f498053093
aqilusman45 <aqilusman45@gmail.com>
2021-12-12 11:02:11 +0000

    fix: releaserc syntax

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/github",
     [
       "@semantic-release/commit-analyzer",
       "@semantic-release/github",
@@ -16,6 +18,14 @@ module.exports = {
             "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
         },
       ],
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
     ],
   ],
   branches: ["main"],
