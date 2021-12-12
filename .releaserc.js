7f021170dacb9c334e9d154b3cb6a6f6ecb191ed
aqilusman45 <aqilusman45@gmail.com>
2021-12-12 17:20:05 +0000

    fix: remove plugin

--- .releaserc.js
+++ .releaserc.js
@@ -1,16 +1,9 @@
 module.exports = {
   plugins: [
+    "@semantic-release/commit-analyzer",
     "@semantic-release/github",
     ["@semantic-release/npm", {
       npmPublish: false
-    }],
-    ["@semantic-release/release-notes-generator", {
-      "parserOpts": {
-        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-      },
-      "writerOpts": {
-        "commitsSort": ["subject", "scope"]
-      }
     }],
     ["@semantic-release/git", {
       "assets": ["package.json"],
