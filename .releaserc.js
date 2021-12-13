12ff8727f6f444d6651007acba8e6e7e4f5056e6
aqilusman45 <aqilusman45@gmail.com>
2021-12-13 22:01:07 +0000

    feat: add note generator

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,17 @@
 module.exports = {
   plugins: [
     [
+      "@semantic-release/release-notes-generator",
+      {
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+        },
+        writerOpts: {
+          commitsSort: ["subject", "scope"],
+        },
+      },
+    ],
+    [
       "@semantic-release/npm",
       {
         npmPublish: false,
